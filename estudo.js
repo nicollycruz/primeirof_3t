77ae6223008db214d9a8913072e000a840d0af10
nicollycruz <110180655+nicollycruz@users.noreply.github.com>
2022-10-20 11:10:54 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,7 +1,6 @@
-let tabuada = 2;
+let tabuada = 8;
 
-
-function escreva(){ 
+function escreva(){
     document.write("Tabuada do " + tabuada + "<br>")
     document.write(tabuada + " x 1 = " + (tabuada*1) + "<br>");
     document.write(tabuada + " x 2 = " + (tabuada*2) + "<br>");
@@ -15,22 +14,54 @@ function escreva(){
     document.write(tabuada + " x 10 = " + (tabuada*10) + "<br>");
 }
 
-let lista = [ "Spessato" , "Duarte" ,"Jesus" , "Raquel"];
+let lista = ["Jorge","Duarte","Waldete","Raquel",];
 
 function mostra(){
     document.write(lista.length + "<br>");
-    for (let i= 0; i < lista.length; i ++){
+    for(let i = 0; i < lista.length; i++){
         document.write(lista[i] + "<br>");
     }
+}
+
+function mostraTabuada(){
+    for(let i = 1; i <= 10; i++){
+        document.write("O valor do i " + i + "<br>");
+    }
+}
+function total(){
+    let v = document.getElementById("valor").value;
+    let j = document.getElementById("juros").value;
+    let t = document.getElementById("meses").value;
+    let r = 0;
+    for(let i =1; i <=t; i++){
+        r = v * (1 + (j/100));
+        document.write("Mês " + i + " - valor: " + r + "<br>");
+        v = r;
+    }
+    document.write("Resultado: " + r);
+
+}
+
+function Totaldasnotas(){
+
+let n1 = document.getElementById("b1").value;
+let n2 = document.getElementById("b2").value;
+let n3 = document.getElementById("b3").value;
+let n4 = document.getElementById("b4").value;
+
+let r = Number(n1)+Number(n2)+Number(n3)+Number(n4)
+
+document.getElementById("resul").innerHTML = "Soma " + r;
+}
+function Media(){
+    let v = document.getElementById("valor").value;
+    let j = document.getElementById("juros").value;
+    let t = document.getElementById("meses").value;
+    let r = 0;
+    for(let i =1; i <=t; i++){
+        r = v * (1 /(j/100));
+        document.write("Mês " + i + " - valor: " + r + "<br>");
+        v = r;
     
 }
-
-function mostratabuada(){
-    for(let i = 1; i<=  10; i++){
-        document.write( "0 valordo i" + i + "<br>");
-    }
-    }
-}
-
-
-
+document.write("Resultado: " + r);
